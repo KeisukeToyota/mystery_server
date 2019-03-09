7f7104f280916abf3cad7e9ea91b432496aa9d38
KeisukeToyota <hm.pudding0715@gmail.com>
2019-03-09 16:51:39 +0900

    fix

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -1,7 +1,18 @@
 class PointsController < ApplicationController
   def index
-    @point = Point.select(:id, :message).find_by!(recent_point_id: params[:recent_point_id])
+    @point = Point.find_by!(recent_point_id: params[:recent_point_id])
 
-    render json: @point
+    render json: Point.select(:id, :message).find_by!(recent_point_id: params[:recent_point_id]) if send_message?(@point.longitude, @point.latitude)
+  end
+
+  private
+
+  def send_message?(lng, lat)
+    y1 = lng * Math::PI / 180
+    x1 = lat * Math::PI / 180
+    y2 = params[:longitude].to_f * Math::PI / 180
+    x2 = params[:latitude].to_f * Math::PI / 180
+    earth_r = 6378.14
+    earth_r * Math.acos(Math.cos(x1) * Math.cos(x2) * Math.cos(y2 - y1) + Math.sin(x1) * Math.sin(x2)) < 0.5
   end
 end
